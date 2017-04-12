b3cf0c586aed123e3de607bec9169f6048f13949
010011100010 <ll3305@nyu.edu>
2017-04-12 15:06:13 -0400

    Mouse point function

---
+++ Week 11/Assets/mousePoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mousePoint : MonoBehaviour {
+	private Camera camera;
+
+	// Use this for initialization
+	void Start () {
+		camera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = camera.ScreenPointToRay (Input.mousePosition);
+		Debug.DrawRay (ray.origin, ray.direction * 1000f, Color.red);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, 1000))
+			{
+			Debug.Log (hit.collider.gameObject.name);
+		}
+	}
+}
